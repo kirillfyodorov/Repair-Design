7ede6c31fb43b6ee1f52b0c792b68a4e71e73d9a
Kirill Fyodorov <kirill-fjodorov00000@yandex.ru>
2019-12-02 22:10:58 +0300

    fix: change gulp on 4th vers and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,116 @@
-var gulp = require("gulp");
-var bs = require("browser-sync");
-var sass = require('gulp-sass');
-var gutil = require( 'gulp-util' );
-var ftp = require( 'vinyl-ftp' );
-var concatCss = require('gulp-concat-css');
-var autoprefixer = require('gulp-autoprefixer');
-var csso = require('gulp-csso');
-var minifyCSS = require('minifycss');
-var plumber = require('gulp-plumber');
+const gulp = require("gulp");
+const browserSync = require('browser-sync').create();
+const sass = require("gulp-sass");
+const concat = require('gulp-concat');
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
+const cleancss = require("gulp-clean-css");
+const minify = require('gulp-minify');
+const imagemin = require("gulp-imagemin");
+const rename = require("gulp-rename");
+const htmlmin = require('gulp-htmlmin');
+const babel = require('gulp-babel');
 
-gulp.task('serve', ['sass'], function() {
 
-    bs.init({
-        server: "./src"
+function bs() {
+
+    sassTask();
+
+    browserSync.init({
+        server: {
+            baseDir: "./src"
+        }
     });
 
-    gulp.watch("src/sass/**/*.sass", ['sass']);
-    gulp.watch("src/*.html").on('change', bs.reload);
-});
+    gulp.watch("src/sass/*.sass").on('change', sassTask);
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("src/sass/**/*.sass")
-        .pipe(sass())
-        .pipe(plumber())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(concatCss("style.css"))
-        .pipe(gulp.dest("src/css"))
-        .pipe(bs.stream());
-});
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("src/css/*.css").on('change', browserSync.reload);
+    gulp.watch("src/js/*.js").on('change', browserSync.reload);
 
-gulp.task('default', ['serve']);
+
+};
+
+function sassTask() {
+    return gulp.src('src/sass/*.sass')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(concat('style.css'))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
+};
+
+// такс для продакшена
+
+function prod() {
+
+    // минимизируем css и перемещаем в dist
+    gulp.src('src/css/style.css')
+    .pipe(cleancss({
+        level: 2
+    }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('dist/css/'));
+
+    //перемещаем style.css, css.map и все sass в dist
+    gulp.src('src/css/style.css')
+    .pipe(gulp.dest('dist/css/'));
+
+    gulp.src('src/css/style.css.map')
+    .pipe(gulp.dest('dist/css/'));
+
+    gulp.src('src/sass/*.sass')
+    .pipe(gulp.dest('dist/sass/'));
+
+    //минимизируем html
+
+    gulp.src('src/index.html')
+    .pipe(htmlmin({
+        collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('dist/'));
+
+    // минимизируем js и перемещаем в dist
+    gulp.src('src/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(concat('script.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(minify({
+        compress: true,
+        ext: '.min.js'
+    }))
+    .pipe(gulp.dest('dist/js/'));
+
+    //минимизируем изображения
+    return gulp.src('src/img/**')
+    .pipe(imagemin([
+        imagemin.gifsicle({
+            interlaced: true,
+            optimizationLevel: 3
+        }),
+        imagemin.jpegtran({
+            progressive: true
+        }),
+        imagemin.optipng({
+            optimizationLevel: 7
+        }),
+        imagemin.svgo({
+            plugins: [{
+                    removeViewBox: true
+                },
+                {
+                    cleanupIDs: true
+                }
+            ]
+        })
+    ]))
+    .pipe(gulp.dest('dist/img/'))
+};
+
+exports.serve = bs;
+exports.prod = prod;
